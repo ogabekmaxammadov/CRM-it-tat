31a7cf2645f44684a114d214895d742b24ae0199
Ogabek Maxammadov <ogabekmaxammadov022006@gmail.com>
2025-08-15 18:17:13 +0500

    ogabek18:17

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,87 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect, useState } from 'react';
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect, useState } from 'react'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 
-import './App.css';
-import HeaderSection from './components/headerSection/HeaderSection';
-import MenuSection from './components/MenuSection/MenuSection';
-import HomePage from './pages/HomePage/HomePage';
-import LoginPage from './pages/LoginPage/LoginPage';
+import './App.css'
+import HeaderSection from './components/headerSection/HeaderSection'
+import MenuSection from './components/MenuSection/MenuSection'
+import HomePage from './pages/HomePage/HomePage'
+import LoginPage from './pages/LoginPage/LoginPage'
 
 function App() {
-  const [collapsed, setCollapsed] = useState(false);
+	const [collapsed, setCollapsed] = useState(false)
 
-  const toggleCollapse = () => {
-    setCollapsed(prev => !prev);
-  };
+	const toggleCollapse = () => {
+		setCollapsed(prev => !prev)
+	}
 
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    localStorage.getItem('isAuthenticated') === 'true'
-  );
+	const [isAuthenticated, setIsAuthenticated] = useState(
+		localStorage.getItem('isAuthenticated') === 'true'
+	)
 
-  const location = useLocation();
+	const location = useLocation()
 
-  useEffect(() => {
-    AOS.init({ duration: 1000, once: true });
-  }, []);
+	useEffect(() => {
+		AOS.init({ duration: 1000, once: true })
+	}, [])
 
-  // 🔹 sidebar toggle bo‘lganda AOS qayta hisoblaydi
-  useEffect(() => {
-    setTimeout(() => {
-      AOS.refresh();
-    }, 50);
-  }, [collapsed]);
+	// 🔹 sidebar toggle bo‘lganda AOS qayta hisoblaydi
+	useEffect(() => {
+		setTimeout(() => {
+			AOS.refresh()
+		}, 50)
+	}, [collapsed])
 
-  const handleLogin = () => {
-    localStorage.setItem('isAuthenticated', 'true');
-    setIsAuthenticated(true);
-  };
+	const handleLogin = () => {
+		localStorage.setItem('isAuthenticated', 'true')
+		setIsAuthenticated(true)
+	}
 
-  const handleLogout = () => {
-    localStorage.removeItem('isAuthenticated');
-    setIsAuthenticated(false);
-  };
+	const handleLogout = () => {
+		localStorage.removeItem('isAuthenticated')
+		setIsAuthenticated(false)
+	}
 
-  return (
-    <div className="container">
-      {location.pathname !== '/login' && (
-        <>
-          <MenuSection
-            collapsed={collapsed}
-            toggleCollapse={toggleCollapse}
-          />
-          <HeaderSection collapsed={collapsed} />
-        </>
-      )}
+	return (
+		<div className='container'>
+			{location.pathname !== '/login' && (
+				<>
+					<MenuSection collapsed={collapsed} toggleCollapse={toggleCollapse} />
+				</>
+			)}
 
-      <div className="pages">
-        <Routes>
-          <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
-          <Route
-            path="/"
-            element={
-              isAuthenticated ? (
-                <HomePage onLogout={handleLogout} collapsed={collapsed} />
-              ) : (
-                <Navigate to="/login" replace />
-              )
-            }
-          />
-        </Routes>
-      </div>
-    </div>
-  );
+			<div className='right'>
+				{location.pathname !== '/login' && (
+					<>
+						<HeaderSection
+							collapsed={collapsed}
+							className={`header-section ${collapsed ? 'collapsed' : ''}`}
+						/>
+					</>
+				)}
+
+				<div className='pages'>
+					<Routes>
+						<Route
+							path='/login'
+							element={<LoginPage onLogin={handleLogin} />}
+						/>
+						<Route
+							path='/'
+							element={
+								isAuthenticated ? (
+									<HomePage onLogout={handleLogout} collapsed={collapsed} />
+								) : (
+									<Navigate to='/login' replace />
+								)
+							}
+						/>
+					</Routes>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/headerSection/HeaderSection.jsx
+++ src/components/headerSection/HeaderSection.jsx
@@ -1,77 +1,83 @@
-import { useEffect, useState } from 'react';
-import { AiOutlineFullscreen, AiOutlineFullscreenExit } from 'react-icons/ai';
-import { FaBell, FaRegPlusSquare } from 'react-icons/fa';
-import { IoSearch } from 'react-icons/io5';
-import { LuClock5 } from 'react-icons/lu';
-import { SlPicture } from 'react-icons/sl';
-import './HeaderSection.css';
+import { useEffect, useState } from 'react'
+import { AiOutlineFullscreen, AiOutlineFullscreenExit } from 'react-icons/ai'
+import { FaBell, FaRegPlusSquare } from 'react-icons/fa'
+import { IoSearch } from 'react-icons/io5'
+import { LuClock5 } from 'react-icons/lu'
+import { SlPicture } from 'react-icons/sl'
+import './HeaderSection.css'
 
-const HeaderSection = ({ collapsed }) => {
-  const [isFull, setIsFull] = useState(false);
-  const [aosOnce, setAosOnce] = useState(true); // faqat 1 marta ishlash uchun
+const HeaderSection = ({ collapsed, className }) => {
+	const [isFull, setIsFull] = useState(false)
+	const [aosOnce, setAosOnce] = useState(true) // faqat 1 marta ishlash uchun
 
-  useEffect(() => {
-    setTimeout(() => setAosOnce(false), 1000); // 1 soniyadan keyin data-aos olib tashlanadi
-  }, []);
+	useEffect(() => {
+		setTimeout(() => setAosOnce(false), 1000) // 1 soniyadan keyin data-aos olib tashlanadi
+	}, [])
 
-  const toggleFullScreen = () => {
-    if (!isFull) {
-      if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen();
-      } else if (document.documentElement.webkitRequestFullscreen) {
-        document.documentElement.webkitRequestFullscreen();
-      } else if (document.documentElement.msRequestFullscreen) {
-        document.documentElement.msRequestFullscreen();
-      }
-      setIsFull(true);
-    } else {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-      }
-      setIsFull(false);
-    }
-  };
+	const toggleFullScreen = () => {
+		if (!isFull) {
+			if (document.documentElement.requestFullscreen) {
+				document.documentElement.requestFullscreen()
+			} else if (document.documentElement.webkitRequestFullscreen) {
+				document.documentElement.webkitRequestFullscreen()
+			} else if (document.documentElement.msRequestFullscreen) {
+				document.documentElement.msRequestFullscreen()
+			}
+			setIsFull(true)
+		} else {
+			if (document.exitFullscreen) {
+				document.exitFullscreen()
+			} else if (document.webkitExitFullscreen) {
+				document.webkitExitFullscreen()
+			} else if (document.msExitFullscreen) {
+				document.msExitFullscreen()
+			}
+			setIsFull(false)
+		}
+	}
+	const headerStyle = {
+		marginLeft: collapsed ? '100px' : '260px',
+	}
 
-  return (
-    <div
-      className={`header-section display-flex ${collapsed ? 'collapsed' : ''}`}
-      {...(aosOnce ? { 'data-aos': 'fade-down' } : {})} // faqat birinchi marta qo‘yiladi
-    >
-      <FaRegPlusSquare className="header-icon" />
-      <div className="search display-flex">
-        <input type="search" placeholder="Qidirish" />
-        <IoSearch className="search-icon header-icon" />
-      </div>
+	return (
+		<div
+			className={`header-section display-flex ${className} ${
+				collapsed ? 'collapsed' : ''
+			}`}
+			style={headerStyle}
+			{...(aosOnce ? { 'data-aos': 'fade-down' } : {})} // faqat birinchi marta qo‘yiladi
+		>
+			<FaRegPlusSquare className='header-icon' />
+			<div className='search display-flex'>
+				<input type='search' placeholder='Qidirish' />
+				<IoSearch className='search-icon header-icon' />
+			</div>
 
-      <div className="right-elements display-flex">
-        <div className="lngs">
-          <h2>uz</h2>
-        </div>
-        <div className="full-ekran">
-          <button
-            onClick={toggleFullScreen}
-            style={{
-              background: 'transparent',
-              cursor: 'pointer',
-              fontSize: '18px',
-            }}
-          >
-            {isFull ? <AiOutlineFullscreenExit /> : <AiOutlineFullscreen />}
-          </button>
-        </div>
-        <LuClock5 className="header-icon clock" />
-        <FaBell className="header-icon clock" />
-        <h4>Asliddin Hakimov</h4>
-        <div className="account">
-          <SlPicture className="account-icon" />
-        </div>
-      </div>
-    </div>
-  );
-};
+			<div className='right-elements display-flex'>
+				<div className='lngs'>
+					<h2>uz</h2>
+				</div>
+				<div className='full-ekran'>
+					<button
+						onClick={toggleFullScreen}
+						style={{
+							background: 'transparent',
+							cursor: 'pointer',
+							fontSize: '18px',
+						}}
+					>
+						{isFull ? <AiOutlineFullscreenExit /> : <AiOutlineFullscreen />}
+					</button>
+				</div>
+				<LuClock5 className='header-icon clock' />
+				<FaBell className='header-icon clock' />
+				<h4>Asliddin Hakimov</h4>
+				<div className='account'>
+					<SlPicture className='account-icon' />
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default HeaderSection;
+export default HeaderSection
